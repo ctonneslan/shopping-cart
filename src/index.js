371274fb23fdcdae13246b8de3f032b2cb29a580
Charlie Tonneslan <cst0520@gmail.com>
2025-07-23 19:42:46 -0400

    Cleaned up the UI

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { renderProductList } from "./modules/dom";
 const app = document.getElementById("app");
 
 app.innerHTML = `
-    <h1>Fake Store</h1>
-    <div id="products"></div>
-    <h2>Cart</h2>
-    <div id="cart"></div>
+    <div id="products-section">
+        <h1>Fake Store</h1>
+        <div id="products"></div>
+    </div>
+    <div id="cart-section">
+        <h2>Cart</h2>
+        <div id="cart"></div>
+    </div>
 `;
 
 renderProductList(document.getElementById("products"));
